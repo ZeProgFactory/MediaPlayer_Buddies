b0a7a016dcdf088e5715122a5f24a2efa6f514a4
Michael Engelmann <zeprogramfactory@outlook.com>
2019-06-08 17:04:30 +0200

    Initial check in ...

---
+++ Samples/MediaPlayer_Test.Forms/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MediaPlayer_Test.Forms
+{
+   public partial class Form1 : Form
+   {
+      public Form1()
+      {
+         InitializeComponent();
+      }
+
+      private void btnPlay_Click(object sender, EventArgs e)
+      {
+         ZPF.Media.MediaPlayer.Current.Play("http://www.zpf.fr/podcast/01.mp3");
+      }
+   }
+}
